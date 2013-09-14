d8d18a3d1541d2daff7daa83abcfcffcb827cae7
Chris Cimaszewski <chris@ninjaerobics.com>
2013-09-14 06:29:49 -0400

    Updated the Logging to include Log4Cocoa

---
+++ Portsmouth/Log4CocoaInitializer.h
@@ -0,0 +1,13 @@
+//
+//  Log4CocoaInitializer.h
+//  Portsmouth
+//
+//  Created by Christopher Cimaszewski on 9/14/13.
+//  Copyright (c) 2013 Ninjaerobics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Log4CocoaInitializer : NSObject
+
+@end
---
+++ Portsmouth/Log4CocoaInitializer.m
@@ -0,0 +1,13 @@
+//
+//  Log4CocoaInitializer.m
+//  Portsmouth
+//
+//  Created by Christopher Cimaszewski on 9/14/13.
+//  Copyright (c) 2013 Ninjaerobics. All rights reserved.
+//
+
+#import "Log4CocoaInitializer.h"
+
+@implementation Log4CocoaInitializer
+
+@end
